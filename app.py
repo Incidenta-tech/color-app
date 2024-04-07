8e4f176c3b3bae6477c0f23d9a0c6e7a185bf395
axsapronov <a@sapronov.me>
2024-04-07 15:44:49 +0300

    Version updated from 1.0.9 to 1.0.10

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 metrics = PrometheusMetrics(app)
 
 # TODO - get version from poetry
-metrics.info("app_info", "Application info", version="1.0.9")
+metrics.info("app_info", "Application info", version="1.0.10")
 
 COLOR_PAIRS = [
     # background-color, text-color
